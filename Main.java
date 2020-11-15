2d48cf46526731ef06697b54129f7e1f60b866d6
Oksana <solomkao@gmail.com>
2020-11-15 13:18:30 +0200

    Improved

--- Main.java
+++ Main.java
@@ -9,11 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //All sucks,folks
+        //Task 1
 
-        Integer array[] = {2, 3, 1, 7, 11};
+        Integer array[] = {0, 2, 3, 1, 7, 11, 8, 9, 9, 17, 3, 5};
+        System.out.println("Before sorting "+Arrays.toString(array));
         sortReverse(array);
+        System.out.println("After sorting "+Arrays.toString(array));
 
+        //Task 2
 
         Integer array_2[] = new Integer[7];
         array_2[0] = -8;
@@ -25,17 +28,20 @@ public class Main {
         array_2[6] = 0;
         System.out.println("The sum of all positive elements is " + sumOfPositive(array_2));
 
+
+        //Task 3
         System.out.println("Average " + averageOfElements(array));
 
-        Integer array_3[] = {1,2,1,2,1};
-        System.out.println("Before replaced duplicates "+Arrays.toString(array_3));;
+        //Task 4
+        Integer array_3[] = {-5, 0, 1, 2, 1, 2, 1, 8, 5, 9, 9};
+        System.out.println("Original array          " + Arrays.toString(array_3));
         replaceDuplicate(array_3);
-        System.out.println("After replaced duplicates "+Arrays.toString(array_3));;
-
+        System.out.println("Duplicates was replaced " + Arrays.toString(array_3));
 
     }
 
     //Create next array of Integers with values: 2,3,1,7,11. Sort them by DESC(reverseOrder).
+
     public static void sortReverse(Integer[] array) {
         Arrays.sort(array, new Comparator<Integer>() {
             @Override
@@ -43,19 +49,19 @@ public class Main {
                 return o2.compareTo(o1);
             }
         });
-        System.out.println(Arrays.toString(array));
     }
 
     //You get an array of numbers(should contain both positive and negative numbers),
     // and return the sum of all the positive ones. (If there are nothing to sum, sum should be 0)
+
     public static int sumOfPositive(Integer array[]) {
         Arrays.sort(array, Comparator.reverseOrder());
         int result = 0;
-        for (Integer integer : array) {
-            if (integer > 0) {
-                result += integer;
+        for (Integer i : array) {
+            if (i > 0) {
+                result += i;
             } else {
-                break;
+                break;  // выходим из цикла при достижении первого отрицтельного числа. их считать не имеет смысла
             }
         }
         return result;
@@ -63,6 +69,7 @@ public class Main {
 
     //You get an array of numbers, return the average of a list of numbers in this array.
     // (Example -  array{1,2,4,1} -> avg = 2)
+
     public static int averageOfElements(Integer array[]) {
         int result = 0;
         for (int i = 0; i < array.length; i++) {
@@ -74,10 +81,16 @@ public class Main {
 
     //You have array {3,2,3,1,4,2,8,3}.
     // Replace all duplicated values by 0. Result should be  next: {3,2,0,1,4,0,8,0}
+
     public static void replaceDuplicate(Integer array[]) {
         int x;
         for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length; j++) {
+            //System.out.println("array[" + i + "] = " + array[i]);
+            if (array[i].equals(0)) {   //чтобы не менять нули на нули.
+                continue;
+            }
+            for (int j = i + 1; j < array.length; j++) {
+                //System.out.println("   array[" + j + "] = " + array[j]);
                 if (array[i].equals(array[j])) {
                     array[j] = 0;
                 }
